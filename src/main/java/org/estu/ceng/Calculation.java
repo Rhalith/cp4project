74a402ab6d975e90423b3ff8e102c4d205d272bc
Rhalith <akmannuhyigit@gmail.com>
2024-04-06 15:30:59 +0300

    Add: calculation class

---
+++ src/main/java/org/estu/ceng/Calculation.java
@@ -0,0 +1,77 @@
+package org.estu.ceng;
+
+public class Calculation {
+
+    private final Properties properties;
+    private final ZChart zChart;
+    private float optimalLotSize;
+    private float reorderPoint;
+    private float zValue; // z-score
+    private float safetyStock;
+    private float averageHoldingCost;
+    private float averageOrderingCost;
+    private float averagePenaltyCost;
+    private float averageTimeBetweenOrders;
+    private float noStockProportion;
+    private float noMetProportion;
+
+    public Calculation(Properties properties, ZChart zChart) {
+        this.properties = properties;
+        this.zChart = zChart;
+    }
+
+    private void calculateOptimalLotSize(Boolean isIteration) {
+        if(isIteration)
+        {
+            optimalLotSize = (float) (2 * properties.getAnnualDemand() * (properties.getOrderingCost() + properties.getPenaltyCost() * properties.getStandardDeviation() * zChart.getLValue(zValue)) / properties.getHoldingCost());
+        }
+        else
+        {
+            optimalLotSize = (float) ((2 * properties.getAnnualDemand() * properties.getOrderingCost()) / properties.getHoldingCost());
+        }
+    }
+
+    private void calculateZ() {
+        zValue = (float) zChart.getZScore(optimalLotSize * properties.getHoldingCost() / (properties.getAnnualDemand() * properties.getPenaltyCost()));
+    }
+
+    private void calculateReorderPoint() {
+        reorderPoint = (float) (properties.getLeadTimeDemand() + zValue * properties.getStandardDeviation());
+    }
+
+    private void iterate(){
+        float oldOptimalLotSize = 1;
+        float newOptimalLotSize = 0;
+        float oldReorderPoint = 1;
+        float newReorderPoint = 0;
+        while (oldOptimalLotSize != newOptimalLotSize || oldReorderPoint != newReorderPoint) {
+            oldOptimalLotSize = optimalLotSize;
+            oldReorderPoint = reorderPoint;
+            calculateOptimalLotSize(true);
+            calculateZ();
+            calculateReorderPoint();
+            newOptimalLotSize = optimalLotSize;
+            newReorderPoint = reorderPoint;
+        }
+    }
+    private void calculateSafetyStock() {
+        safetyStock = (float) (reorderPoint - properties.getLeadTimeDemand());
+    }
+    private void calculateAverageCosts(){
+        averageHoldingCost = (float) (properties.getHoldingCost() * (optimalLotSize / 2 + safetyStock));
+        averageOrderingCost = (float) (properties.getOrderingCost() * properties.getAnnualDemand() / optimalLotSize);
+        averagePenaltyCost = (float) (properties.getPenaltyCost() * (properties.getAnnualDemand() * properties.getStandardDeviation() * zChart.getLValue(zValue) / optimalLotSize));
+    }
+    
+    private void calculateAverageTime(){
+        averageTimeBetweenOrders = (float) (optimalLotSize / properties.getAnnualDemand());
+    }
+
+    private void calculateStockProportions(){
+        noStockProportion = (float) (1 - (optimalLotSize * properties.getHoldingCost() / (properties.getAnnualDemand() * properties.getPenaltyCost())));
+    }
+
+    private void calculateNotMetProportion(){
+        noMetProportion = (float) (properties.getStandardDeviation() * zChart.getLValue(zValue) / optimalLotSize);
+    }
+}
